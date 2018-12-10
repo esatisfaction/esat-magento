cb8d2ec40d6fb7a8f1dade6544e601e1972154bd
lysandrosgiorgis <45609073+lysandrosgiorgis@users.noreply.github.com>
2018-12-10 20:08:51 +0200

    Add files via upload
    Files compatible with style and array changed to []

--- app/code/community/Esat/Esatisfaction/Model/Resource/Item/Collection.php
+++ app/code/community/Esat/Esatisfaction/Model/Resource/Item/Collection.php
@@ -1,7 +1,9 @@
-<?php 
-class Esat_Esatisfaction_Model_Resource_Item_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract {
+<?php
+
+class Esat_Esatisfaction_Model_Resource_Item_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract
+{
     protected function _construct()
     {
-            $this->_init('esatisfaction/item');
+        $this->_init('esatisfaction/item');
     }
 }
--- app/code/community/Esat/Esatisfaction/Model/System/Config/Source/Delivery/Pipeline.php
+++ app/code/community/Esat/Esatisfaction/Model/System/Config/Source/Delivery/Pipeline.php
@@ -1,60 +1,61 @@
-<?php 
+<?php
+
 class Esat_Esatisfaction_Model_System_Config_Source_Delivery_Pipeline
 {
-	public function toOptionArray()
-	{
-		$esat_helper 	= Mage::helper('esatisfaction/data');	
-		$token 			= $esat_helper->getToken();
-		$application_id = $esat_helper->getApplicationId();
+    public function toOptionArray()
+    {
+        $esat_helper = Mage::helper('esatisfaction/data');
+        $token = $esat_helper->getToken();
+        $application_id = $esat_helper->getApplicationId();
+		$pipelines = [];
 		
-		if(!empty($token) && !empty($application_id)){	
-			$questionnaire_id = $esat_helper->getDeliveryQuestionnaireId();
-			
-			if(!empty($questionnaire_id)){				
-				$ch = curl_init();
-				
-				curl_setopt($ch, CURLOPT_URL, "https://api.e-satisfaction.com/v3.0/q/questionnaire/".$questionnaire_id."/pipeline");
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-				curl_setopt($ch, CURLOPT_HEADER, FALSE);
+        if (!empty($token) && !empty($application_id)) {
+            $questionnaire_id = $esat_helper->getDeliveryQuestionnaireId();
 
-				curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-				  "Content-Type: application/json",
-				  "Accept: application/json",
-				  "esat-auth: ".$token
-				));
+            if (!empty($questionnaire_id)) {
+                $ch = curl_init();
 
-				$response = curl_exec($ch);
-				curl_close($ch);
-				$response_data = json_decode($response,true);
-				
-				if($httpcode == 200){	
-					$pipelines = array();
-					foreach($response_data as $result){
-						$pipelines[] = array(
-							'value' => $result['pipeline_id'], 
-							'label' => $result['title']
-						);
-					}
-				}else{
-					$pipelines[] = array(
-						'value' => 0, 
-						'label' => $response_data['message']
-					);
-				}
-			}else{
-				$pipelines[] = array(
-					'value' => 0, 
-					'label' => 'You must first select a Delivery Questionnaire'
-				);
-			}
-		}else{
-			$pipelines[] = array(
-				'value' => 0, 
-				'label' => 'You must give Authentication Token & Application ID'
-			);
-		}
-		
-		return $pipelines;
-	}
+                curl_setopt($ch, CURLOPT_URL, 'https://api.e-satisfaction.com/v3.0/q/questionnaire/'.$questionnaire_id.'/pipeline');
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_HEADER, false);
+
+                curl_setopt($ch, CURLOPT_HTTPHEADER, [
+                  'Content-Type: application/json',
+                  'Accept: application/json',
+                  'esat-auth: '.$token
+                ]);
+
+                $response = curl_exec($ch);
+				$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+                curl_close($ch);
+                $response_data = json_decode($response, true);
+
+                if ($httpcode == 200) {
+                    foreach ($response_data as $result) {
+                        $pipelines[] = [
+                            'value' => $result['pipeline_id'],
+                            'label' => $result['title']
+                        ];
+                    }
+                } else {
+                    $pipelines[] = [
+                        'value' => 0,
+                        'label' => $response_data['message']
+                    ];
+                }
+            } else {
+                $pipelines[] = [
+                    'value' => 0,
+                    'label' => 'You must first select a Delivery Questionnaire'
+                ];
+            }
+        } else {
+            $pipelines[] = [
+                'value' => 0,
+                'label' => 'You must give Authentication Token & Application ID'
+            ];
+        }
+
+        return $pipelines;
+    }
 }
-?>
