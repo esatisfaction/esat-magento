f308ffa9aac8da43c63fd047ef2742fb87dd2fe9
Ioannis Papikas <papikas.ioan@gmail.com>
2019-04-04 16:29:36 +0300

    Improve code style, remove multiple encapsulations

--- app/code/community/Esat/Esatisfaction/Model/System/Config/Source/Pickup/Pipeline.php
+++ app/code/community/Esat/Esatisfaction/Model/System/Config/Source/Pickup/Pipeline.php
@@ -13,52 +13,59 @@ class Esat_Esatisfaction_Model_System_Config_Source_Pickup_Pipeline
         $helperData = Mage::helper('esatisfaction/data');
         $token = $helperData->getToken();
         $applicationId = $helperData->getApplicationId();
-        $pipelines = [];
 
-        if (!empty($token) && !empty($applicationId)) {
-            $questionnaireId = $helperData->getPickupQuestionnaireId();
+        // Validate token and application id
+        if (empty($token) || empty($applicationId)) {
+            return [
+                [
+                    'value' => 0,
+                    'label' => 'You must provide an Application Id and an Authentication Token',
+                ],
+            ];
+        }
 
-            if (!empty($questionnaireId)) {
-                $ch = curl_init();
-
-                curl_setopt($ch, CURLOPT_URL, sprintf('https://api.e-satisfaction.com/v3.0/q/questionnaire/%s/pipeline', $questionnaireId));
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_HEADER, false);
-
-                curl_setopt($ch, CURLOPT_HTTPHEADER, [
-                    'Content-Type: application/json',
-                    'Accept: application/json',
-                    'esat-auth: ' . $token,
-                ]);
-
-                $response = curl_exec($ch);
-                $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-                curl_close($ch);
-                $response_data = json_decode($response, true);
-
-                if ($httpcode == 200) {
-                    foreach ($response_data as $result) {
-                        $pipelines[] = [
-                            'value' => $result['pipeline_id'],
-                            'label' => $result['title'],
-                        ];
-                    }
-                } else {
-                    $pipelines[] = [
-                        'value' => 0,
-                        'label' => $response_data['message'],
-                    ];
-                }
-            } else {
-                $pipelines[] = [
+        // Check pickup questionnaire
+        $questionnaireId = $helperData->getPickupQuestionnaireId();
+        if (empty($questionnaireId)) {
+            return [
+                [
                     'value' => 0,
                     'label' => 'You must first select a Pickup Questionnaire',
-                ];
-            }
-        } else {
+                ],
+            ];
+        }
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, sprintf('https://api.e-satisfaction.com/v3.1/q/questionnaire/%s/pipeline', $questionnaireId));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'Accept: application/json',
+            'esat-auth: ' . $token,
+        ]);
+
+        $response = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        $response_data = json_decode($response, true);
+
+        // Get API error message
+        if ($httpCode != 200) {
+            return [
+                [
+                    'value' => 0,
+                    'label' => $response_data['message'],
+                ],
+            ];
+        }
+
+        // Get pipelines
+        $pipelines = [];
+        foreach ($response_data as $result) {
             $pipelines[] = [
-                'value' => 0,
-                'label' => 'You must give Authentication Token & Application ID',
+                'value' => $result['pipeline_id'],
+                'label' => $result['title'],
             ];
         }
 
